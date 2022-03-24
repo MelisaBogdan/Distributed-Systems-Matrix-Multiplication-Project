0377f847d382630df3f02d366ec144494a0af6ef
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-03-24 21:24:59 +0000

    Update RestServiceApplication.java

--- grpc-server/src/main/java/com/example/grpc/rest/RestServiceApplication.java
+++ grpc-server/src/main/java/com/example/grpc/rest/RestServiceApplication.java
@@ -1,4 +1,4 @@
-package com.dscoursework.restservice;
+package com.CW-DS.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
