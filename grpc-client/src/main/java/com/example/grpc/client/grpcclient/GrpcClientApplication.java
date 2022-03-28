0304fec2b96aee928eacde70f2d35741ee8ed687
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-03-28 18:23:39 +0100

    fix

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
@@ -13,6 +13,16 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 import com.example.uploadingfiles.storage.StorageProperties;
 import com.example.uploadingfiles.storage.StorageService;
+package com.example.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.uploadingfiles.storage.StorageProperties;
+import com.example.uploadingfiles.storage.StorageService;
 
 @SpringBootApplication
 public class GrpcClientApplication extends SpringBootServletInitializer {
