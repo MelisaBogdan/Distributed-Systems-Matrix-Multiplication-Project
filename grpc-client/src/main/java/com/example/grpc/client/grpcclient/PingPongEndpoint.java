5d943d91a4dfa07915d1421f3962cbb901f5ab47
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-03-30 14:59:52 +0100

    Update PingPongEndpoint.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -29,8 +29,8 @@ public class PingPongEndpoint {
 		return grpcClientService.add();
 	}
 	
-	@PostMapping("/upload")
-	public FileUploadResponse fileUpload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException{
+	// @PostMapping("/upload")
+	/*public FileUploadResponse fileUpload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException{
 		return grpcClientService.fileUpload(file);
-	}
+	}*/
 }
