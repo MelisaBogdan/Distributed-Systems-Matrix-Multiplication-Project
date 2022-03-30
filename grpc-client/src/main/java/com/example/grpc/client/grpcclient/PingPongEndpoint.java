9376dc192d3a8b6e023fa4b8aad55acd998dad6a
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-03-30 15:41:13 +0100

    Update PingPongEndpoint.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -29,8 +29,8 @@ public class PingPongEndpoint {
 		return grpcClientService.add();
 	}
 	
-	@GetMapping("/upload")
+	/*@GetMapping("/upload")
 	public String upload() {
 		return grpcClientService.upload();
-	}
+	}*/
 }
