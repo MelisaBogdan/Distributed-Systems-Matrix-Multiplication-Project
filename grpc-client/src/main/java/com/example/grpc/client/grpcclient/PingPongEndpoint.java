0302b11eb0b4e0ed5dcc5ed6379e06eb5ff5b321
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-03-31 13:02:42 +0100

    Update PingPongEndpoint.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/PingPongEndpoint.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model;
+
 import java.io.IOException;
 
 @Controller
