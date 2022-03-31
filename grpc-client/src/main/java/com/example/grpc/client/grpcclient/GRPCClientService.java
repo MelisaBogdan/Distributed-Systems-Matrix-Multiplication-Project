958aed366888690bc94ab2ea27c801a3ad3091d9
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-03-31 12:53:40 +0100

    Update GRPCClientService.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -44,8 +44,8 @@ public class GRPCClientService {
 		String resp= A.getC00()+" "+A.getC01()+"<br>"+A.getC10()+" "+A.getC11()+"\n";
 		return resp;
     }
-    public String upload(){
-	   return null;
-    }
+//     public String upload(){
+// 	   return null;
+//     }
 
 }
