6a556a642761ee469efc7a03488f1c26dd510484
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-17 22:50:47 +0300

    Update FileUploadController.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/uploadingfiles/FileUploadController.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/uploadingfiles/FileUploadController.java
@@ -23,7 +23,8 @@ import com.example.uploadingfiles.storage.StorageFileNotFoundException;
 import com.example.uploadingfiles.storage.StorageService;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File; 
