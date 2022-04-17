78b15d32a2ce8d3b909e9db32eb673dabf67411b
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-17 21:43:15 +0300

    Update GrpcClientApplication.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
@@ -1,51 +1,51 @@
-package com.example.grpc.client.grpcclient;
+// package com.example.grpc.client.grpcclient;
 
-import com.example.grpc.server.grpcserver.HelloRequest;
-import com.example.grpc.server.grpcserver.HelloResponse;
-import com.example.grpc.server.grpcserver.HelloServiceGrpc;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import net.devh.boot.grpc.client.inject.GrpcClient;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+// import com.example.grpc.server.grpcserver.HelloRequest;
+// import com.example.grpc.server.grpcserver.HelloResponse;
+// import com.example.grpc.server.grpcserver.HelloServiceGrpc;
+// import io.grpc.ManagedChannel;
+// import io.grpc.ManagedChannelBuilder;
+// import net.devh.boot.grpc.client.inject.GrpcClient;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-@SpringBootApplication
-public class GrpcClientApplication extends SpringBootServletInitializer {
+// @SpringBootApplication
+// public class GrpcClientApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrpcClientApplication.class, args);
-		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-				.usePlaintext()
-				.build();
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(GrpcClientApplication.class, args);
+// 		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+// 				.usePlaintext()
+// 				.build();
 
-		HelloServiceGrpc.HelloServiceBlockingStub stub
-				= HelloServiceGrpc.newBlockingStub(channel);
+// 		HelloServiceGrpc.HelloServiceBlockingStub stub
+// 				= HelloServiceGrpc.newBlockingStub(channel);
 
-		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-				.setFirstName("Baeldung")
-				.setLastName("gRPC")
-				.build());
+// 		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+// 				.setFirstName("Baeldung")
+// 				.setLastName("gRPC")
+// 				.build());
 
-		System.out.println(helloResponse);
+// 		System.out.println(helloResponse);
 
-		channel.shutdown();*/
+// 		channel.shutdown();*/
 
-		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-				.usePlaintext()
-				.build();
+// 		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+// 				.usePlaintext()
+// 				.build();
 
-		HelloServiceGrpc.HelloServiceBlockingStub stub
-				= HelloServiceGrpc.newBlockingStub(channel);
+// 		HelloServiceGrpc.HelloServiceBlockingStub stub
+// 				= HelloServiceGrpc.newBlockingStub(channel);
 
-		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-				.setFirstName("Baeldung")
-				.setLastName("gRPC")
-				.build());
+// 		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+// 				.setFirstName("Baeldung")
+// 				.setLastName("gRPC")
+// 				.build());
 
-		channel.shutdown();*/
-//		SpringApplication.run(GrpcClientApplication.class, args);
-	}
+// 		channel.shutdown();*/
+// //		SpringApplication.run(GrpcClientApplication.class, args);
+// 	}
 
-}
+// }
