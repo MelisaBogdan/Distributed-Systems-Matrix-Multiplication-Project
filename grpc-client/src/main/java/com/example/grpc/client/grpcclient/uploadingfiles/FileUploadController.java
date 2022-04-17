fa54a95c966604db7e5ad9f9bb549c9e899fbacb
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-17 18:30:04 +0300

    try2

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/uploadingfiles/FileUploadController.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/uploadingfiles/FileUploadController.java
@@ -69,8 +69,23 @@ public class FileUploadController {
 
 		storageService.store(file);
 		
-		// add stuff here
-		String matrixA_temp = txt2String(file);
+
+// 		String matrixA_temp = txt2String(file);
+		StringBuilder result = new StringBuilder();
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String s = null;
+                    while ((s = br.readLine()) != null) {
+                        result.append(System.lineSeparator() + s);
+                    }
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String matrixA_temp = result.toString();
+		
+		
+		
 // 		int[][] matrixA = convertToMatrix(matrixA_temp);
 		
 // 		if(matrixA.length != matrixA[0].length){
@@ -94,20 +109,20 @@ public class FileUploadController {
 	}
 	
 	// Get matrix string from the file
-        public static String txt2String(File file) {
-                StringBuilder result = new StringBuilder();
-                try {
-                    BufferedReader br = new BufferedReader(new FileReader(file));
-                    String s = null;
-                    while ((s = br.readLine()) != null) {
-                        result.append(System.lineSeparator() + s);
-                    }
-                    br.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return result.toString();
-        }
+//         public static String txt2String(File file) {
+//                 StringBuilder result = new StringBuilder();
+//                 try {
+//                     BufferedReader br = new BufferedReader(new FileReader(file));
+//                     String s = null;
+//                     while ((s = br.readLine()) != null) {
+//                         result.append(System.lineSeparator() + s);
+//                     }
+//                     br.close();
+//                 } catch (Exception e) {
+//                     e.printStackTrace();
+//                 }
+//                 return result.toString();
+//         }
 	
 	 public static int[][] convertToMatrix(String m){
 
