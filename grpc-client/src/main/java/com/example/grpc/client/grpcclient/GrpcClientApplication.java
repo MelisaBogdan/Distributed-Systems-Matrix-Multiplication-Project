d51b980d755417a3195481bd3d155d9b707437e1
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-17 18:02:06 +0300

    Update GrpcClientApplication.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
@@ -29,45 +29,31 @@ public class GrpcClientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrpcClientApplication.class, args);
-// 		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-// 				.usePlaintext()
-// 				.build();
+		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
+				.usePlaintext()
+				.build();
+		HelloServiceGrpc.HelloServiceBlockingStub stub
+				= HelloServiceGrpc.newBlockingStub(channel);
+		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+				.setFirstName("Baeldung")
+				.setLastName("gRPC")
+				.build());
+		System.out.println(helloResponse);
+		channel.shutdown();*/
 
-// 		HelloServiceGrpc.HelloServiceBlockingStub stub
-// 				= HelloServiceGrpc.newBlockingStub(channel);
-
-// 		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-// 				.setFirstName("Baeldung")
-// 				.setLastName("gRPC")
-// 				.build());
-
-// 		System.out.println(helloResponse);
-
-// 		channel.shutdown();*/
-
-// 		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
-// 				.usePlaintext()
-// 				.build();
-
-// 		HelloServiceGrpc.HelloServiceBlockingStub stub
-// 				= HelloServiceGrpc.newBlockingStub(channel);
-
-// 		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
-// 				.setFirstName("Baeldung")
-// 				.setLastName("gRPC")
-// 				.build());
-
-// 		channel.shutdown();*/
-// 		SpringApplication.run(GrpcClientApplication.class, args);
-// 	}
-
-// 	@Bean
-// 	CommandLineRunner init(StorageService storageService) {
-// 		return (args) -> {
-// 			storageService.deleteAll();
-// 			storageService.init();
-// 		};
+		/*ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+				.usePlaintext()
+				.build();
+		HelloServiceGrpc.HelloServiceBlockingStub stub
+				= HelloServiceGrpc.newBlockingStub(channel);
+		HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+				.setFirstName("Baeldung")
+				.setLastName("gRPC")
+				.build());
+		channel.shutdown();*/
+//		SpringApplication.run(GrpcClientApplication.class, args);
 	}
+	
 
 }
 
