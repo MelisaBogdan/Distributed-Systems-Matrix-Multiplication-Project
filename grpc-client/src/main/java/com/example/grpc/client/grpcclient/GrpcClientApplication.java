36ed59382be001465015c95b61fb13c5ed53adf5
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-17 21:42:18 +0300

    Update GrpcClientApplication.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/GrpcClientApplication.java
@@ -10,19 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import org.springframework.context.annotation.Bean;
-import com.example.uploadingfiles.storage.StorageProperties;
-import com.example.uploadingfiles.storage.StorageService;
-package com.example.uploadingfiles;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import com.example.uploadingfiles.storage.StorageProperties;
-import com.example.uploadingfiles.storage.StorageService;
 
 @SpringBootApplication
 public class GrpcClientApplication extends SpringBootServletInitializer {
@@ -61,13 +48,4 @@ public class GrpcClientApplication extends SpringBootServletInitializer {
 //		SpringApplication.run(GrpcClientApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(StorageService storageService) {
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
-	}
-
 }
-
