1d116231e78cad15b53d6e12e97e0706f081302a
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-18 23:21:42 +0300

    Update FileUploadController.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/uploadingfiles/FileUploadController.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/uploadingfiles/FileUploadController.java
@@ -95,7 +95,7 @@ public class FileUploadController {
 			String matrixTwo = txt2String(dest).split(matrixSymbols)[1];
 			
 			redirectAttributes.addFlashAttribute("message",
-					"You successfully uploaded " +" "+ file.getOriginalFilename() + " and results is "+matrixOne + ' and '+ matrixTwo+" !!");
+					"You successfully uploaded " +" "+ file.getOriginalFilename() + " and results is "+matrixOne + " and "+ matrixTwo +" !!");
 		}
 
 		return "redirect:/";
