f6e0349f53736f480f4fd9ecd327a5a7889760ad
Melisa Bogdan <melisa.bogdan@gmail.com>
2022-04-24 21:06:39 +0300

    nothing much

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/uploadingfiles/storage/IncompatibleMatrixException.java
+++
@@ -1,7 +0,0 @@
-package com.example.grpc.client.grpcclient.exceptions;
-
-public class IncompatibleMatrixException extends Exception{
-    public  IncompatibleMatrixException(String errorMessage){
-        super(errorMessage);
-    }
-}
