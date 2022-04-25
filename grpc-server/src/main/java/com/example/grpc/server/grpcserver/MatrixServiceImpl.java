e87f0abdb031c1c40dcc34fe4678c4298884a96a
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-25 15:51:56 +0300

    no need for addBlock

--- grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
+++ grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
@@ -6,16 +6,16 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @GrpcService
 
 class MatrixServiceImpl extends MatrixServiceGrpc.MatrixServiceImplBase {
-    @Override
-    public void addBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
-	{
-		System.out.println("Request received from client:\n" + request);
-		int C00=request.getA00()+request.getB00();
+//     @Override
+//     public void addBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
+// 	{
+// 		System.out.println("Request received from client:\n" + request);
+// 		int C00=request.getA00()+request.getB00();
 
-		MatrixReply response = MatrixReply.newBuilder().setC00(C00).build();
-		reply.onNext(response);
-		reply.onCompleted();
-	}
+// 		MatrixReply response = MatrixReply.newBuilder().setC00(C00).build();
+// 		reply.onNext(response);
+// 		reply.onCompleted();
+// 	}
     
 	@Override
     public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
