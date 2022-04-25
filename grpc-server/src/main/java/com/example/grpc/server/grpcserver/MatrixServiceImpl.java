a2e1d9898da1244cba7d29e3e309df52c7d6069e
Melisa Andreea Bogdan <34380133+MelisaBogdan@users.noreply.github.com>
2022-04-25 23:33:49 +0300

    Update MatrixServiceImpl.java

--- grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
+++ grpc-server/src/main/java/com/example/grpc/server/grpcserver/MatrixServiceImpl.java
@@ -9,69 +9,22 @@ class MatrixServiceImpl extends MatrixServiceGrpc.MatrixServiceImplBase {
     @Override
     public void addBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
 	{
-// 		System.out.println("Request received from client:\n" + request);
-// 		int C00=request.getA00()+request.getB00();
+		System.out.println("Request received from client:\n" + request);
+		int C00=request.getA00()+request.getB00();
 
-// 		MatrixReply response = MatrixReply.newBuilder().setC00(C00).build();
-// 		reply.onNext(response);
-// 		reply.onCompleted();
-	    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-				.usePlaintext()
-				.build();		
-		ArrayList<MatrixReply>rep = new ArrayList<>();
-		MatrixServiceGrpc.MatrixServiceBlockingStub stub = MatrixServiceGrpc.newBlockingStub(channel);
-		//loop through list of blocks
-		for(int counter =0; counter<m1Blocked.size();counter++)
-		{
-			//retrieve each block to be added
-			int[][] takeBlock1 = m1Blocked.get(counter);
-			int[][] takeBlock2 = m2Blocked.get(counter);
-			MatrixReply A = stub.addBlock(MatrixRequest.newBuilder()
-				.setA00(takeBlock1[0][0])
-				.setA01(takeBlock1[0][1])
-				.setA10(takeBlock1[1][0])
-				.setA11(takeBlock1[1][1])
-				.setB00(takeBlock2[0][0])
-				.setB01(takeBlock2[0][1])
-				.setB10(takeBlock2[1][0])
-				.setB11(takeBlock2[1][1])
-				.build());
-				rep.add(A);
-				//build a matrixreply arraylist of blocks
-				
-		}
-		String resp = getResponse(rep);
-		return resp;
+		MatrixReply response = MatrixReply.newBuilder().setC00(C00).build();
+		reply.onNext(response);
+		reply.onCompleted();
 	}
     
 	@Override
     public void multiplyBlock(MatrixRequest request, StreamObserver<MatrixReply> reply)
     {
-// 		System.out.println("Request received from client:\n" + request);
-// 		int C00=request.getA00()*request.getB00();
+		int C00=request.getA00()*request.getB00();
 
-// 		MatrixReply response = MatrixReply.newBuilder().setC00(C00).build();
-// 		reply.onNext(response);
-// 		reply.onCompleted();
-	        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090)
-				.usePlaintext()
-				.build();
-		MatrixServiceGrpc.MatrixServiceBlockingStub stub = MatrixServiceGrpc.newBlockingStub(channel);
-		MatrixReply A = stub.multiplyBlock(MatrixRequest.newBuilder()
-				.setA00(m1[0][0])
-				.setA01(m1[0][1])
-				.setA10(m1[1][0])
-				.setA11(m1[1][1])
-				.setB00(m2[0][0])
-				.setB01(m2[0][1])
-				.setB10(m2[1][0])
-				.setB11(m2[1][1])
-				.build());
-		//works using 2x2 matrices
-		String resp = A.getC00() + A.getC01() + A.getC10() + A.getC11() + "";
-		print(resp);
-		
-		return resp;
+		MatrixReply response = MatrixReply.newBuilder().setC00(C00).build();
+		reply.onNext(response);
+		reply.onCompleted();
     }
 }
 
